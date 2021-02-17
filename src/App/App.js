629030734c98d19885c2277a18f024f377e12938
Luke Mason <lukjmason@gmail.com>
2021-02-17 09:30:58 -0700

    Pass reservations from fetch requests as props to ReservationCard component

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import fetchRequests from '../fetch-requests.js'
+import fetchRequests from '../fetch-requests.js';
+import ReservationCard from './reservation-card.js';
 
 class App extends Component {
   constructor() {
@@ -30,7 +31,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-
+          < ReservationCard reservations={this.state.reservations} />
         </div>
       </div>
     )
---
+++ src/App/reservation-card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ReservationCard (props) {
+  console.log(props);
+  return ('hehe')
+}
+
+export default ReservationCard;
