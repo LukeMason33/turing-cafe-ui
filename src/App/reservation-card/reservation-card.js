02590ab0990cccc2e504087f9a7a295e0c77ccdf
Luke Mason <lukjmason@gmail.com>
2021-02-17 10:17:40 -0700

    Initia; styling for cards

--- src/App/reservation-card/reservation-card.js
+++ src/App/reservation-card/reservation-card.js
@@ -7,8 +7,8 @@ function ReservationCard (props) {
     <article className="reservation-card" id={props.reservation.id}>
       <h1 className="res-name">{props.reservation.name}</h1>
       <h2 className="res-date">{props.reservation.date}</h2>
-      <h2 className="res-time">{props.reservation.time}</h2>
-      <h2 className="res-guests">{props.reservation.number}</h2>
+      <h2 className="res-time">{`${props.reservation.time} pm`}</h2>
+      <h2 className="res-guests">{`Number of guests: ${props.reservation.number}`}</h2>
       <button className="cancel-res-button">Cancel</button>
     </article>
   )
